73e60f0561208db100912961454aa35bd69a0da2
Josh Waller <mdx.programs@gmail.com>
2023-08-19 12:32:09 -0400

    prepping for render hosting test

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,3 @@
-export default ({ env }) => ({
-  proxy: true,
-  url: env('APP_URL'), // Sets the public URL of the application.
-  app: { 
-    keys: env.array('APP_KEYS')
-  },
+module.exports = ({ env }) => ({
+  url: env("RENDER_EXTERNAL_URL"),
 });
