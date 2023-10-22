6c4f9948f2aea95bacdd613f6090405ddd912cd7
Josh Waller <mdx.programs@gmail.com>
2023-10-22 17:38:50 -0400

    adds volunteer form submissions model

---
+++ src/api/volunteer-form-submission/routes/volunteer-form-submission.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * volunteer-form-submission router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::volunteer-form-submission.volunteer-form-submission');
