31bba98c7b56ba4a05529af1ca90bd6d659f8d13
Josh Waller <mdx.programs@gmail.com>
2023-10-25 19:39:28 -0400

    retry adding volunteer form in cms

---
+++ src/api/volunteer-form-submission/controllers/volunteer-form-submission.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * volunteer-form-submission controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::volunteer-form-submission.volunteer-form-submission');
