2091f3ee4bd904e0e6cd2275446cb55be9e49fbc
Josh Waller <mdx.programs@gmail.com>
2023-10-27 10:53:08 -0400

    adding form submission email trigger send

---
+++ src/api/contact-form-submission/content-types/contact-form-submission/lifecycles.js
@@ -0,0 +1,29 @@
+module.exports = {
+  async afterCreate(event) {
+    try {
+      await strapi.plugins["email"].services.email.send({
+        to: "engage@trinitylifeministry.com",
+        from: "engage@trinitylifeministry.com", // e.g. single sender verification in SendGrid
+        replyTo: "engage@trinitylifeministry.com",
+        subject: "Trinity Life Ministry - Contact Form Submission",
+        text: event.params.message,
+        html: "",
+      });
+    } catch (err) {
+      console.log(err.body);
+    }
+
+    try {
+      await strapi.plugins["email"].services.email.send({
+        to: event.params.email,
+        from: "engage@trinitylifeministry.com", // e.g. single sender verification in SendGrid
+        replyTo: "engage@trinitylifeministry.com",
+        subject: "Thank you for contacting Trinity Liife Ministry",
+        text: "We have received your information and will be in contact within the next 48 hours. Thank you for reaching out!",
+        html: "",
+      });
+    } catch (err) {
+      console.log(err.body);
+    }
+  },
+};
