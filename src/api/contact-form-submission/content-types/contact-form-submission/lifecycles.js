a82217289f871d8cb86984b9222ad42464839234
Josh Waller <mdx.programs@gmail.com>
2023-11-01 14:15:15 -0400

    fixing property usage in contact afterCreate

--- src/api/contact-form-submission/content-types/contact-form-submission/lifecycles.js
+++ src/api/contact-form-submission/content-types/contact-form-submission/lifecycles.js
@@ -1,37 +1,29 @@
 module.exports = {
   async afterCreate(event) {
-    console.log("---EVENT---");
-    console.log(event);
-
-    if (event?.result) {
-      console.log("---PARAMS---");
-      console.log(event.result);
+    try {
+      await strapi.plugins["email"].services.email.send({
+        to: "engage@trinitylifeministry.com",
+        from: "engage@trinitylifeministry.com", // e.g. single sender verification in SendGrid
+        replyTo: "engage@trinitylifeministry.com",
+        subject: "Trinity Life Ministry - Contact Form Submission",
+        text: event.result.message,
+        html: "",
+      });
+    } catch (err) {
+      console.log(err);
     }
 
-    // try {
-    //   await strapi.plugins["email"].services.email.send({
-    //     to: "engage@trinitylifeministry.com",
-    //     from: "engage@trinitylifeministry.com", // e.g. single sender verification in SendGrid
-    //     replyTo: "engage@trinitylifeministry.com",
-    //     subject: "Trinity Life Ministry - Contact Form Submission",
-    //     text: event.params.message,
-    //     html: "",
-    //   });
-    // } catch (err) {
-    //   console.log(err);
-    // }
-
-    // try {
-    //   await strapi.plugins["email"].services.email.send({
-    //     to: event.params.email,
-    //     from: "engage@trinitylifeministry.com", // e.g. single sender verification in SendGrid
-    //     replyTo: "engage@trinitylifeministry.com",
-    //     subject: "Thank you for contacting Trinity Liife Ministry",
-    //     text: "We have received your information and will be in contact within the next 48 hours. Thank you for reaching out!",
-    //     html: "",
-    //   });
-    // } catch (err) {
-    //   console.log(err);
-    // }
+    try {
+      await strapi.plugins["email"].services.email.send({
+        to: event.result.email,
+        from: "engage@trinitylifeministry.com", // e.g. single sender verification in SendGrid
+        replyTo: "engage@trinitylifeministry.com",
+        subject: "Thank you for contacting Trinity Liife Ministry",
+        text: "We have received your information and will be in contact within the next 48 hours. Thank you for reaching out!",
+        html: "",
+      });
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
