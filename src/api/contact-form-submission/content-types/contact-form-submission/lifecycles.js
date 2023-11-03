b57dfe4795ffccd2d2939b85d7b46a42732b500a
Josh Waller <mdx.programs@gmail.com>
2023-11-03 10:55:16 -0400

    testing afterCreate with aircode

--- src/api/contact-form-submission/content-types/contact-form-submission/lifecycles.js
+++ src/api/contact-form-submission/content-types/contact-form-submission/lifecycles.js
@@ -1,27 +1,11 @@
 module.exports = {
   async afterCreate(event) {
     try {
-      await strapi.plugins["email"].services.email.send({
-        to: "engage@trinitylifeministry.com",
-        from: "engage@trinitylifeministry.com", // e.g. single sender verification in SendGrid
-        replyTo: "engage@trinitylifeministry.com",
-        subject: "Trinity Life Ministry - Contact Form Submission",
-        text: event.result.message,
-        html: "",
-      });
-    } catch (err) {
-      console.log(err);
-    }
-
-    try {
-      await strapi.plugins["email"].services.email.send({
-        to: event.result.email,
-        from: "engage@trinitylifeministry.com", // e.g. single sender verification in SendGrid
-        replyTo: "engage@trinitylifeministry.com",
-        subject: "Thank you for contacting Trinity Liife Ministry",
-        text: "We have received your information and will be in contact within the next 48 hours. Thank you for reaching out!",
-        html: "",
-      });
+      await fetch("https://pfrrthx5az.us.aircode.run/email", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(event.result),
+      }).then((res) => console.log(res.json()));
     } catch (err) {
       console.log(err);
     }
